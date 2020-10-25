97d2990d92d44435a78202e9c44e874740935bb4
luca.vitucci <luca.vitucci@numbrs.com>
2020-10-25 19:04:33 +0100

    Link the identifier to the provider instance

--- src/main/kotlin/provider/DefaultProvider.kt
+++ src/main/kotlin/provider/DefaultProvider.kt
@@ -6,8 +6,7 @@ import java.util.*
  * Default provider implementation making use of a random number generator to provide a (mostly) unique identifier.
  */
 class DefaultProvider : Provider {
+    private val instanceId = UUID.randomUUID().toString().take(7)
 
-    override fun get(): String =
-        UUID.randomUUID().toString().take(7)
-
+    override fun get(): String = instanceId
 }
